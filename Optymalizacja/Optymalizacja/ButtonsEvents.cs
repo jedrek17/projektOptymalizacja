c34a872094652bf5c558495d2ef00774dc82f845
jedras <jedrek_wnuk@wp.pl>
2016-04-02 01:14:40 +0200

    Dodalem dwa nowe pliki, buttonsEvents w ktorym bedziemy wrzucac obsluge zdarzen przyciskow, i Variables, gdzie przechowywane beda zmienne, nie jest to moze najlepsze rozwiazanie, ale duzo latwiej jest sie tak potem odnalesc w kodzie

---
+++ Optymalizacja/Optymalizacja/ButtonsEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optymalizacja
+{
+    partial class Form1
+    {
+        // tutaj bedziemy wrzucac funkcje zwiazane z obsluga zdarzen na przyciskach
+
+
+    }
+}
---
+++ Optymalizacja/Optymalizacja/Variables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optymalizacja
+{
+    partial class Form1
+    {
+        // tutaj bedziemy umieszczac roznego rodzaju zmienne wykorzystywane w programie 
+
+
+    }
+}
