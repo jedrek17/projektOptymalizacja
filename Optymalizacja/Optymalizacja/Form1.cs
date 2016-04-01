f387a078f9711b4a186b400a8af5a6b1b7ff0a9a
FilipDepta <deptafilip@gmail.com>
2016-04-02 00:26:43 +0200

    Pierwsza próbna aktualizacja

--- Optymalizacja/Optymalizacja/Form1.cs
+++ Optymalizacja/Optymalizacja/Form1.cs
@@ -16,5 +16,10 @@ namespace Optymalizacja
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Kliknięty";
+        }
     }
 }
