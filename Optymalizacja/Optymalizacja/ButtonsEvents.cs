a93dd82daa1995180733b941bc4e7eb1bcad9f43
FilipDepta <deptafilip@gmail.com>
2016-04-29 00:49:32 +0200

    Dalsze prace nad klasami cPoint, cSimplex i cSimplexSolver. Oczywiście końca jeszcze nie widać ;)

--- Optymalizacja/Optymalizacja/ButtonsEvents.cs
+++ Optymalizacja/Optymalizacja/ButtonsEvents.cs
@@ -10,21 +10,26 @@ namespace Optymalizacja
     {
         //funkcje związane z obsługą zdarzeń
 
-        double rownanieTestowe(double x1, double x2)
+        public double rownanieTestowe(double x1, double x2)
         {
             return Math.Pow(x1-4,2) + Math.Pow(x2 - 2, 2);  //Funkcja ma min w punkcie (4,2)
         }
     }
 
 
-
     class cPoint
     {
-        public double[] wsp;
-        public double y;
+        public double[] wsp;    //współrzędne
+        public double y;    //wartość funkcji w punkcie
+
         public cPoint(int n)    //n - ile współrzędnych ma jeden punkt
         {
             wsp = new double[n];
+        }
+
+        public void liczP() //oblicza wartość funkcji w punkcie
+        {
+            y = Math.Pow(wsp[0] - 4, 2) + Math.Pow(wsp[1] - 2, 2);  //nie umiałem skorzystać z funkcji testowej (wywala coś dziwnego), to musiałem ją po prostu wkleić...
         }
     }
 
@@ -32,10 +37,17 @@ namespace Optymalizacja
     class cSimplex
     {
         public cPoint[] pkt;
-        public cSimplex(int n)
+
+        public cSimplex(int n) //n - z ilu punktów składa się simpleks
         {
             for(int i=0; i<n; i++)
                 pkt[i] = new cPoint(n-1);   //simpleks ma rozmiar o jeden większy niż ilość wymiarów zadania
+        }
+
+        public void liczS() //oblicza wartość każdego punktu simpleksu
+        {
+            for (int i = 0; i < pkt.Length; i++)
+                pkt[i].liczP();
         }
     }
 
@@ -45,11 +57,10 @@ namespace Optymalizacja
         public byte n;          //n-wymiarowa funkcja
         public cSimplex simpPocz;    //simpleks początkowy
 
-
-        public cSimplexSolver(byte n)
+        public cSimplexSolver(byte n)   //n - ilość wymiarów funkcji
         {
             this.n = n;
-            simpPocz = new cSimplex(n+1);
+            simpPocz = new cSimplex(n+1);   //simpleks zawsze o 1 więcej punktów
         }
     }
 }
