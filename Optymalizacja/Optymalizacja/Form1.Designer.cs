8f3037483631374c923182c01ed4fc881c2cffc0
jedras <jedrek_wnuk@wp.pl>
2016-05-08 15:19:11 +0200

    zmieniłem wygląd aplikacji, dodałem pole do wprowadzania funkcji < jeszcze do uzgodnienia> dodałem też pole, w którym wyświetlany będzie wykres wynikowy

--- Optymalizacja/Optymalizacja/Form1.Designer.cs
+++ Optymalizacja/Optymalizacja/Form1.Designer.cs
@@ -35,11 +35,24 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btCalculate = new System.Windows.Forms.Button();
+            this.tbInsertFunction = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxGraph = new System.Windows.Forms.PictureBox();
+            this.btPreviousStep = new System.Windows.Forms.Button();
+            this.btNextStep = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(86, 205);
+            this.button1.Location = new System.Drawing.Point(66, 108);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -49,7 +62,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(64, 52);
+            this.textBox1.Location = new System.Drawing.Point(41, 19);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
@@ -57,7 +70,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(64, 79);
+            this.textBox2.Location = new System.Drawing.Point(41, 46);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
@@ -66,7 +79,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(64, 117);
+            this.label1.Location = new System.Drawing.Point(19, 78);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 3;
@@ -75,7 +88,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(64, 130);
+            this.label2.Location = new System.Drawing.Point(19, 91);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 4;
@@ -84,7 +97,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(64, 143);
+            this.label3.Location = new System.Drawing.Point(19, 104);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 5;
@@ -92,28 +105,120 @@
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(64, 156);
+            this.label4.Location = new System.Drawing.Point(19, 117);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 0;
             this.label4.Text = "label4";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(800, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(216, 157);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tymczasowe - jakies okienka <?>";
+            // 
+            // btCalculate
+            // 
+            this.btCalculate.Location = new System.Drawing.Point(698, 61);
+            this.btCalculate.Name = "btCalculate";
+            this.btCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btCalculate.TabIndex = 8;
+            this.btCalculate.Text = "Wylicz";
+            this.btCalculate.UseVisualStyleBackColor = true;
+            // 
+            // tbInsertFunction
+            // 
+            this.tbInsertFunction.Location = new System.Drawing.Point(108, 35);
+            this.tbInsertFunction.Name = "tbInsertFunction";
+            this.tbInsertFunction.Size = new System.Drawing.Size(665, 20);
+            this.tbInsertFunction.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Wprowadz funkcję";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btCalculate);
+            this.groupBox2.Controls.Add(this.tbInsertFunction);
+            this.groupBox2.Location = new System.Drawing.Point(15, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(779, 101);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Dane wejściowe";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btNextStep);
+            this.groupBox3.Controls.Add(this.btPreviousStep);
+            this.groupBox3.Controls.Add(this.pictureBoxGraph);
+            this.groupBox3.Location = new System.Drawing.Point(15, 119);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(779, 700);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Wyniki";
+            // 
+            // pictureBoxGraph
+            // 
+            this.pictureBoxGraph.Location = new System.Drawing.Point(133, 19);
+            this.pictureBoxGraph.Name = "pictureBoxGraph";
+            this.pictureBoxGraph.Size = new System.Drawing.Size(640, 640);
+            this.pictureBoxGraph.TabIndex = 0;
+            this.pictureBoxGraph.TabStop = false;
+            // 
+            // btPreviousStep
+            // 
+            this.btPreviousStep.Location = new System.Drawing.Point(721, 665);
+            this.btPreviousStep.Name = "btPreviousStep";
+            this.btPreviousStep.Size = new System.Drawing.Size(23, 23);
+            this.btPreviousStep.TabIndex = 1;
+            this.btPreviousStep.Text = "<";
+            this.btPreviousStep.UseVisualStyleBackColor = true;
+            // 
+            // btNextStep
+            // 
+            this.btNextStep.Location = new System.Drawing.Point(750, 665);
+            this.btNextStep.Name = "btNextStep";
+            this.btNextStep.Size = new System.Drawing.Size(23, 23);
+            this.btNextStep.TabIndex = 2;
+            this.btNextStep.Text = ">";
+            this.btNextStep.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(992, 896);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -126,6 +231,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btCalculate;
+        private System.Windows.Forms.TextBox tbInsertFunction;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btNextStep;
+        private System.Windows.Forms.Button btPreviousStep;
+        private System.Windows.Forms.PictureBox pictureBoxGraph;
     }
 }
 
